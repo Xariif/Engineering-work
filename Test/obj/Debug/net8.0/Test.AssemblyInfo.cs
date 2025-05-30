9cdeffd466378d6daa8ab9d4b88032a39fbe4e9f
Jakub Filiks <jakubfiliks7@gmail.com>
2025-05-30 21:16:30 +0200

    CORS

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05307a708017454de1d824c927c8f24e049b28f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14a3fb69b3e57cf461e3754543679625e787201a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
