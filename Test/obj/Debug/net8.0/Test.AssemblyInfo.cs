8dc744c7daea0f564b7a629f113680a4bae18345
Jakub Filiks <jakubfiliks7@gmail.com>
2025-09-14 14:44:55 +0200

    Refactor deployment workflow to use Docker for backend and frontend services

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1f125c87bbb981031466740544a3edb4a61f432")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824c6ab45385bea216551f4508319b5200dd40e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
