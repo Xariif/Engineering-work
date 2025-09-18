e0079d30879140366ba388d51b4eece5eac7e145
Jakub Filiks <jakub.filiks@trackeo.io>
2025-09-18 20:34:26 +0200

    test

--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8de6c308cf9e2f26b1432dd2f379d9031b061bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7f31e83edf4078847f04674c2d9a77f0bdb009")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
